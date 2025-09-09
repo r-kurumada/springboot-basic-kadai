2f19c8083d966670759e6f2ed9b79adb4e2c7946
r-kurumada <yuna1192god@ezweb.ne.jp>
2025-09-09 17:50:48 +0900

    Add files via upload

---
+++ spring_kadai_from/src/main/java/com/example/springtutorial/SpringKadaiFromApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadaiFromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadaiFromApplication.class, args);
+	}
+
+}
